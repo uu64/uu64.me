c7310d6c52cc3fdc2100604f7225a7a3aa4ea942
uu64 <yusuke.t.0808@gmail.com>
2020-07-04 20:33:12 +0900

    fix: postcss settings

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,39 @@
 module.exports = {
-  plugins: ["tailwindcss", "autoprefixer"],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    // "@fullhuman/postcss-purgecss": {
+    //   content: ["./components/**/*.js", "./pages/**/*.js"],
+    //   defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    // },
+    // "postcss-flexbugs-fixes": {},
+    // "postcss-preset-env": {
+    //   autoprefixer: {
+    //     flexbox: "no-2009",
+    //   },
+    //   stage: 3,
+    //   features: {
+    //     "custom-properties": false,
+    //   },
+    // },
+    ...(process.env.NODE_ENV === "production"
+      ? {
+          // "@fullhuman/postcss-purgecss": {
+          //   content: ["./components/**/*.js", "./pages/**/*.js"],
+          //   defaultExtractor: (content) =>
+          //     content.match(/[\w-/:]+(?<!:)/g) || [],
+          // },
+          "postcss-flexbugs-fixes": {},
+          "postcss-preset-env": {
+            autoprefixer: {
+              flexbox: "no-2009",
+            },
+            stage: 3,
+            features: {
+              "custom-properties": false,
+            },
+          },
+        }
+      : {}),
+  },
 };
