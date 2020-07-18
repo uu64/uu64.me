fcc7146177da7317f5e3eb177306b51caa036939
uu64 <yusuke.t.0808@gmail.com>
2020-07-18 19:03:32 +0900

    fix: lint error

--- pages/[...id].tsx
+++ pages/[...id].tsx
@@ -16,7 +16,10 @@ const Post: React.FC<Props> = (props: Props) => {
     () => import(`@/posts/${(id as string[]).join("/")}.mdx`) as any
   );
   return (
-    <TPostsLayout frontMatter={frontMatter} path={`posts/${(id as string[]).join("/")}`}>
+    <TPostsLayout
+      frontMatter={frontMatter}
+      path={`posts/${(id as string[]).join("/")}`}
+    >
       <MDX />
     </TPostsLayout>
   );
