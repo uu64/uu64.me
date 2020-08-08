221cceb9786890d146b2f0ec6044602959a9f404
uu64 <yusuke.t.0808@gmail.com>
2020-08-08 22:23:51 +0900

    fix: bug

--- components/atoms/ABlockquote.tsx
+++ components/atoms/ABlockquote.tsx
@@ -1,23 +1,15 @@
 import React from "react";
-import styled from "styled-components";
 
 interface Props {
   children: React.ReactNode;
 }
 
-const BlocoQuote = styled.blockquote`
-  > p {
-    padding-top: 1rem !important;
-    padding-bottom: 1rem !important;
-  }
-`;
-
 const ABlockquote: React.FC<Props> = (props: Props) => {
   const { children } = props;
   return (
-    <BlocoQuote className="border-l-8 border-gray-500 bg-gray-300 text-gray-700 px-4 mb-4">
+    <blockquote className="border-l-8 border-gray-500 bg-gray-300 text-gray-700 px-4 mb-4">
       {children}
-    </BlocoQuote>
+    </blockquote>
   );
 };
 
