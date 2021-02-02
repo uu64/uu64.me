1854d54e9205efd76da3b0b288c605ffa0f25e8a
uu64 <yusuke.t.0808@gmail.com>
2021-02-03 08:59:16 +0900

    add: nord color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,23 @@ module.exports = {
         normal: "#1e56a0",
         light: "#f8f8ff",
         white: "#f6f6f6",
+        // Nord Color Pallets
+        // https://www.nordtheme.com/
+        night0: "#2e3440",
+        night1: "#3b4252",
+        night2: "#434c5e",
+        night3: "#4c566a",
+        snow0: "#d8dee9",
+        snow1: "#e5e9f0",
+        snow2: "#eceff4",
+        frost0: "#8fbcbb",
+        frost1: "#88c0d0",
+        frost2: "#81a1c1",
+        aurora0: "#bf616a",
+        aurora1: "#d08770",
+        aurora2: "#ebcb8b",
+        aurora3: "#a3be8c",
+        aurora4: "#b48ead",
       },
     },
     fontFamily: {
