90e2ce9266c535386d95076975d897a346dd69d6
uu64 <yusuke.t.0808@gmail.com>
2021-02-05 23:54:42 +0900

    fix: footer

--- components/organisms/OFooter.tsx
+++ components/organisms/OFooter.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import siteconfig from "@/siteconfig.json";
+import styled from "styled-components";
+
+const Svg = styled.svg`
+  margin: -1px;
+`;
 
 const OFooter: React.FC = () => {
   return (
     <>
-      <svg
+      <Svg
         className="pt-12 fill-night1 stroke-night1"
         version="1.1"
         viewBox="0 0 1228 80.448"
@@ -14,7 +19,7 @@ const OFooter: React.FC = () => {
           className="border-custom-night1"
           d="m0.096151 25.992c182.86-27.18 266.83-25.886 345.19-25.886s433.22 32.243 543.82 45.3c175.4 20.709 338.76-1.2943 338.76-1.2943v36.24h-1227.8z"
         />
-      </svg>
+      </Svg>
       <div className="bg-custom-night1 text-center pt-4 pb-8">
         {siteconfig.copyright}
       </div>
